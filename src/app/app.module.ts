9a684bac5063cc9a477d5492d7cbe8704d1e4ab2
Abdiel Reyes <abykings1@hotmail.com>
2021-05-18 22:35:15 -0500

    Fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MiComponenteComponent } from './mi-componente/mi-componente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MiComponenteComponent
+    
   ],
   imports: [
     BrowserModule,
